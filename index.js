6c7ea0bc1988433705046ee451299e060b55d0a5
Hieu Ho <55455388+qqq12598@users.noreply.github.com>
2021-06-21 18:09:53 +0700

    ...

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static("./public"));
 app.use(express.json());
 
-let port = process.env.PORT || 1337;
+let port = process.env.PORT || 8080;
 
 require('./middlewares/routes.mdw')(app);
 
